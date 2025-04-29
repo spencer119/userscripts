5c1282443060db90be5143c8e2cb92f10d2525f5
Spencer Hamilton <spencernhamilton@gmail.com>
2025-04-29 12:17:48 -0600

    auto commit msg

--- .github/prompts/Google Search Buttons.prompt.md
+++ .github/prompts/Google Search Buttons.prompt.md
@@ -1,15 +1,15 @@
 # Overview
 
-Your task is to assist in creating, editing, debugging, etc. a browser userscript for the userscript manager extension TamperMonkey.
+Your task is to assist in creating, editing, debugging, etc. a browser userscript for the userscript manager extension TamperMonkey. You will write the userscript in the file #file:google-filters.js .
 
-The userscript is going to be for searches on "google.com", so the userscript should be for urls that match anything starting with "<https://www.google.com/search>". 
+The userscript is going to be for searches on "google.com", so the userscript should be for urls that match anything starting with "<https://www.google.com/search>".
 
 # Userscript Details
 
 The userscript will be modifying the search filter toolbar at the top of the google search page below the search box.
 You will be adding additional buttons to the search filter toolbar on the google search results page. All the buttons for the search filter toolbar are in a div with role="list" (Alternatively the list div has a full XPath "/html/body/div[3]/div/div[4]/div/div/div/div/div[1]/div/div/div/div/div[1]/div").
 
-You will be adding buttons at the end of the search filter toolbar, after the last filter but before the More button with 3 dots. It will be the last listItem in the list div, and it will be before the More button div at the end of the list (you can find the more div at the very end of the toolbar with the XPath "/html/body/div[3]/div/div[4]/div/div/div/div/div[1]/div/div/div/div/div[1]/div/div[8]")
+You will be adding buttons at the end of the search filter toolbar, after the last filter but before the More button with 3 dots. It will be the last listitem in the list div, and it will be before the More button div at the end of the list (you can find the more div at the very end of the toolbar with the XPath "/html/body/div[3]/div/div[4]/div/div/div/div/div[1]/div/div/div/div/div[1]/div/div[8]")
 
 # New Buttons to Add
 
@@ -31,3 +31,47 @@ The new buttons you will be adding to the toolbar are:
   - For example, if I searched "best react frameworks" and im on the URL "<https://www.google.com/search?q=sitebest+react+frameworks>". Clicking the GitHub button would navigate me to the page "<https://www.google.com/search?q=site%3Agithub.com%3A+best+react+frameworks>"
 
 # Context and Example Source Code
+
+Example html source code of what the expected html on a google search has been provided.
+In list.html there is example source code for the list div which contains listitem divs for each filter. This example list has the following search filters in the toolbar from left to right:
+AI Mode, All, Images, Shopping, Videos, Short videos, Forums, : More
+
+In this example the userscript would change the toolbar to have two more listitem divs before the More button (the More button is a unique listitem because it has a jsaction and jscontroller attribute). The new order of the list would be:
+AI Mode, All, Images, Shopping, Videos, Short videos, Forums, Reddit, GitHub, : More
+
+# Overview
+
+Your task is to assist in creating, editing, debugging, etc. a browser userscript for the userscript manager extension TamperMonkey. You will write the userscript in new file google-filters.user.js
+
+The userscript is going to be for automatically adding new filter buttons to every search result Google. So, the userscript should be for urls that match google search queries.
+
+# Userscript Details
+
+The userscript will be modifying the search filter toolbar at the top of the google search page below the search box.
+You will be adding additional buttons to the search filter toolbar on the google search results page. All the buttons for the search filter toolbar are in a div with role="list"
+
+To familiarize yourself with the structure of the web page. Analyze
+
+You will be appending new buttons at the end of the list div. You will append the new buttons in the second to last position of the list. The new buttons will be added after the last listitem
+
+# Context and Example Source Code
+
+You have been provided with example source files
+
+# General HTML Structure
+
+- The first main parent div is a div with class="main" id="main". All other needed divs will be within this one. This will be referred to as the main div. You can see an example of the source code in example.html
+- The next element you will need is the list div. The div has a role="list" and contains listitem children elements. Each of the listitem divs here is a button for a search filter. This div will be referred to as the list div.
+  - The last element in the list div has attributes like: `<div jscontroller="xdV1C" class="SF7xd" role="listitem" jsaction="rcuQ6b:npT2md;KyPa0e:Y0y4c">`. This is the More button in the toolbar.
+
+## Reddit Search Filter (New Button)
+
+- The button should say "Reddit".
+- When the Reddit search filter button is clicked, append text to the beginning of the search query. You will be adding "site:reddit.com " to the beginning of the google search query.
+  - For example, if I searched "go lang" and im on the URL "<https://www.google.com/search?q=go+lang>". Clicking the reddit button would navigate me to the page "<https://www.google.com/search?q=site%3Areddit.com+go+lang>"
+
+## GitHub Search Filter (New Button)
+
+- The button should say "GitHub".
+- When the GitHub search filter button is clicked, append text to the beginning of the search query. You will be adding "site:github.com " to the beginning of the google search query.
+  - For example, if I searched "best react frameworks" and im on the URL "<https://www.google.com/search?q=sitebest+react+frameworks>". Clicking the GitHub button would navigate me to the page "<https://www.google.com/search?q=site%3Agithub.com%3A+best+react+frameworks>"
--- google-filters.user.js
+++ google-filters.user.js
@@ -1,143 +1,213 @@
 // ==UserScript==
-// @name         Google Search Filters for Reddit and GitHub
+// @name         Google Search - Reddit & GitHub Filters
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Add Reddit and GitHub filters to Google search results
-// @author       spencer119
+// @version      1.0
+// @description  Adds Reddit and GitHub filter buttons to Google search results
+// @author       You
 // @match        https://www.google.com/search*
+// @match        https://google.com/search*
+// @icon         https://www.google.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
 (function () {
   "use strict";
 
-  // Try to add the custom filters periodically until successful or max attempts reached
-  let attempts = 0;
-  const maxAttempts = 20;
-
-  const checkInterval = setInterval(function () {
-    attempts++;
-    if (addCustomFilters() || attempts >= maxAttempts) {
-      clearInterval(checkInterval);
-    }
-  }, 500);
-
-  function addCustomFilters() {
-    // Find the search filter toolbar (div with role="list")
-    const filterToolbar = document.querySelector(
-      'div[role="list"][style="display: contents"]'
-    );
-    if (!filterToolbar) return false;
-
-    // Check if we've already added our custom filters
-    if (
-      document.querySelector('[data-custom-filter="reddit"]') ||
-      document.querySelector('[data-custom-filter="github"]')
-    ) {
-      return true;
-    }
-
-    // Find the "More" button at the end of the toolbar
-    const moreButton = Array.from(filterToolbar.children).find((child) => {
-      return (
-        child.classList.contains("SF7xd") ||
-        child.querySelector(".SF7xd") ||
-        child.querySelector('[jscontroller="xdV1C"]')
-      );
-    });
-
-    if (!moreButton) return false;
-
-    // Get an existing button to copy its style
-    const existingButton = document.querySelector(
-      'div[role="listitem"]:not(.SF7xd)'
-    );
-    if (!existingButton) return false;
-
-    // Create and add Reddit filter button
-    const redditButton = createFilterButton(
-      "Reddit",
-      "site:reddit.com ",
-      existingButton
-    );
-    if (redditButton) {
-      redditButton.setAttribute("data-custom-filter", "reddit");
-      filterToolbar.insertBefore(redditButton, moreButton);
-    }
-
-    // Create and add GitHub filter button
-    const githubButton = createFilterButton(
-      "GitHub",
-      "site:github.com ",
-      existingButton
-    );
-    if (githubButton) {
-      githubButton.setAttribute("data-custom-filter", "github");
-      filterToolbar.insertBefore(githubButton, moreButton);
-    }
-
-    return true;
+  // Add custom CSS to ensure the pointer cursor on hover
+  function addCustomStyles() {
+    const styleElement = document.createElement("style");
+    styleElement.textContent = `
+            .custom-filter-button a {
+                cursor: pointer !important;
+            }
+        `;
+    document.head.appendChild(styleElement);
   }
 
-  function createFilterButton(text, sitePrefix, templateButton) {
-    // Create a list item div
+  // Function to create filter buttons
+  function createFilterButton(text, site) {
+    // Create the container div with role="listitem"
     const listItem = document.createElement("div");
     listItem.setAttribute("role", "listitem");
+    listItem.className = "custom-filter-button"; // Add custom class for styling
 
-    // Create the anchor element with the same classes as template
+    // Create the anchor element
     const anchor = document.createElement("a");
-    const templateAnchor = templateButton.querySelector("a");
-    if (templateAnchor) {
-      anchor.className = templateAnchor.className;
-    } else {
-      anchor.className = "nPDzT T3FoJb";
-    }
-
-    // Set up the anchor properties
+    anchor.className = "nPDzT T3FoJb";
     anchor.setAttribute("role", "link");
-    anchor.href = "#";
 
-    // Create the inner div with the same classes as template
+    // Create the inner div for the button text
     const innerDiv = document.createElement("div");
-    const templateInnerDiv = templateAnchor
-      ? templateAnchor.querySelector("div")
-      : null;
-    if (templateInnerDiv) {
-      innerDiv.className = templateInnerDiv.className;
-    } else {
-      innerDiv.className = "YmvwI";
-    }
-
+    innerDiv.className = "YmvwI";
+    innerDiv.setAttribute("jsname", "bVqjv");
     innerDiv.textContent = text;
 
-    // Add click handler
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      modifySearchQuery(sitePrefix);
-    });
-
-    // Put it all together
+    // Append the inner div to the anchor
     anchor.appendChild(innerDiv);
+
+    // Append the anchor to the list item
     listItem.appendChild(anchor);
+
+    // Add click event listener
+    anchor.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Get current search query from URL
+      const urlParams = new URLSearchParams(window.location.search);
+      const currentQuery = urlParams.get("q") || "";
+
+      // Prevent adding the site filter multiple times
+      const sitePrefix = `site:${site} `;
+      let newQuery = currentQuery;
+
+      if (!currentQuery.includes(`site:${site}`)) {
+        newQuery = sitePrefix + currentQuery;
+      }
+
+      // Update URL and navigate
+      urlParams.set("q", newQuery);
+      window.location.href =
+        window.location.pathname + "?" + urlParams.toString();
+    });
 
     return listItem;
   }
 
-  function modifySearchQuery(prefix) {
-    // Get the current search query from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const currentQuery = urlParams.get("q") || "";
-
-    // Check if the prefix is already in the query
-    if (!currentQuery.includes(prefix)) {
-      // Add the prefix to the beginning of the query
-      const newQuery = prefix + currentQuery;
-      urlParams.set("q", newQuery);
-
-      // Navigate to the new search URL
-      window.location.href = `${window.location.origin}${
-        window.location.pathname
-      }?${urlParams.toString()}`;
+  // Function to add the buttons to the page
+  function addFilterButtons() {
+    // First try to find the hdtb-sc element by ID
+    const hdtbScElement = document.getElementById("hdtb-sc");
+    if (!hdtbScElement) {
+      console.log("Google Filters: hdtb-sc element not found");
+      return;
     }
+
+    // Try multiple approaches to find the list div
+    let listContainer = null;
+
+    // Approach 1: Try direct XPath
+    try {
+      const xpathResult = document.evaluate(
+        '//*[@id="hdtb-sc"]/div/div/div[1]/div',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      );
+      if (xpathResult && xpathResult.singleNodeValue) {
+        const potentialListContainer =
+          xpathResult.singleNodeValue.querySelector('div[role="list"]');
+        if (potentialListContainer) {
+          listContainer = potentialListContainer;
+        }
+      }
+    } catch (e) {
+      console.log("Google Filters: XPath error:", e);
+    }
+
+    // Approach 2: Try to find it by role within hdtb-sc
+    if (!listContainer) {
+      listContainer = hdtbScElement.querySelector('div[role="list"]');
+    }
+
+    // Approach 3: Try to find any div with role="list" in the document
+    if (!listContainer) {
+      const allListContainers = document.querySelectorAll('div[role="list"]');
+      // Find the one that has listitems with YmvwI class (filter buttons)
+      for (const container of allListContainers) {
+        if (container.querySelector('div[role="listitem"] div.YmvwI')) {
+          listContainer = container;
+          break;
+        }
+      }
+    }
+
+    if (!listContainer) {
+      console.log("Google Filters: List container not found");
+      return;
+    }
+
+    // Find the More button (it has the distinct Lu57id class)
+    const moreButton = Array.from(
+      listContainer.querySelectorAll('div[role="listitem"]')
+    ).find((item) => item.querySelector(".Lu57id"));
+
+    if (!moreButton) {
+      console.log("Google Filters: More button not found");
+      return;
+    }
+
+    // Check if our buttons are already there
+    const existingRedditButton = Array.from(
+      listContainer.querySelectorAll('div[role="listitem"]')
+    ).find((item) => {
+      const textDiv = item.querySelector("div.YmvwI");
+      return textDiv && textDiv.textContent === "Reddit";
+    });
+
+    if (existingRedditButton) {
+      console.log("Google Filters: Buttons already added");
+      return;
+    }
+
+    // Create Reddit filter button
+    const redditButton = createFilterButton("Reddit", "reddit.com");
+
+    // Create GitHub filter button
+    const githubButton = createFilterButton("GitHub", "github.com");
+
+    // Insert the buttons before the More button
+    listContainer.insertBefore(redditButton, moreButton);
+    listContainer.insertBefore(githubButton, moreButton);
+
+    console.log("Google Filters: Added Reddit and GitHub buttons");
+  }
+
+  // Execute when DOM is fully loaded
+  function init() {
+    // Add custom styles first
+    addCustomStyles();
+
+    // Delay execution slightly to ensure the page is fully rendered
+    setTimeout(() => {
+      addFilterButtons();
+
+      // Sometimes Google loads content dynamically, so we also set up a mutation observer
+      const observer = new MutationObserver((mutations) => {
+        // Check if hdtb-sc exists and doesn't have our custom buttons yet
+        const hdtbScElement = document.getElementById("hdtb-sc");
+        if (hdtbScElement) {
+          const listDivs = document.querySelectorAll('div[role="list"]');
+          let needsButtons = true;
+
+          for (const listDiv of listDivs) {
+            const items = listDiv.querySelectorAll(
+              'div[role="listitem"] div.YmvwI'
+            );
+            for (const item of items) {
+              if (item.textContent === "Reddit") {
+                needsButtons = false;
+                break;
+              }
+            }
+            if (!needsButtons) break;
+          }
+
+          if (needsButtons) {
+            addFilterButtons();
+          }
+        }
+      });
+
+      // Start observing the document body for changes
+      observer.observe(document.body, { childList: true, subtree: true });
+    }, 1000); // Wait 1 second for the page to fully render
+  }
+
+  // Run the script
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
   }
 })();
